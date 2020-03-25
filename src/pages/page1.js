00758340378dedf615e8dd0379b11dc38d961140
zhangli <zhangli@singularityclub.net>
2020-03-25 14:04:15 +0800

    test

--- src/pages/page1.js
+++ src/pages/page1.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default class HomePage extends React.Component {
     render () {
         return (
-            <h1>Page1 !</h1>
+            <h1>Page1 !test</h1>
         )
     }
 }
