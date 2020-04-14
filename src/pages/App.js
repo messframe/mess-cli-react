9ca30401291e67834239ea5671b49a92ae5aa720
justwiner <392110917@qq.com>
2020-04-14 21:12:22 +0800

    feat: use history router

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,18 +2,18 @@ import HomePage from './homePage'
 import Page1 from './page1'
 import Page2 from './page2'
 import React from 'react'
-import {Route,HashRouter, Switch} from 'react-router-dom'
+import {Route,BrowserRouter, Switch} from 'react-router-dom'
 
 export default class App extends React.Component {
     render () {
         return (
-            <HashRouter>
+            <BrowserRouter>
                 <Switch>
                     <Route exact path="/mess_react" component={HomePage}/>
                     <Route exact path="/mess_react/page1" component={Page1}/>
                     <Route exact path="/mess_react/page2" component={Page2}/>
                 </Switch>
-            </HashRouter>
+            </BrowserRouter>
         )
     }
 }
