fb4f198cf7d016dc60e799eceb2dba7850e95035
justwiner <392110917@qq.com>
2020-04-15 16:50:58 +0800

    feat: set up routing library at local runtime

--- src/localDev.js
+++ src/localDev.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './pages/App';
+import {resetSingleSpa} from './utils/singleSpa'
+import MessRouter from 'mess-router'
+
+resetSingleSpa({
+    router: new MessRouter('hash')
+})
 
 const app = document.createElement('div');
 document.body.appendChild(app);
